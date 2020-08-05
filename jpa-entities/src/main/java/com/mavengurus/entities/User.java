cb5724ea5b5d4f29d339b27e04fb9f04ee5d674f
ASKRIGhassen <gh.askri@gmail.com>
2020-08-05 17:36:22 +0200

    first modules jpa entities

---
+++ jpa-entities/src/main/java/com/mavengurus/entities/User.java
@@ -0,0 +1,22 @@
+package com.mavengurus.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
